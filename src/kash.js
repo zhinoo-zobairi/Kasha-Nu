8f1742e4e1ed18b0017a88e8a356b0f6a21cdfc5
jinno-kursani <145927421+jinno-kursani@users.noreply.github.com>
2023-10-01 18:20:11 +0200

    added time using dt

--- src/kash.js
+++ src/kash.js
@@ -1,4 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    hours = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemp(response) {
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let humidityElement = document.querySelector("#humidity");
@@ -10,6 +34,7 @@ function displayTemp(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
 }
+let city = "Diyarbakır";
 let apiKey = "a0a183380df8741e35218ccc59e2fe87";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Diyarbakır&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemp);
